f781092c5022ff88685cb2c9d1588ebb522d65a2
Christian Ortner <christian.ortner@tugraz.at>
2023-11-30 17:45:12 +0100

    WIP add opencast service to load videos
    TODO: all datafields that can be mapped to oer have to be processed.

--- classes/module.php
+++ classes/module.php
@@ -25,8 +25,12 @@
 
 namespace oermod_opencast;
 
+use local_oer\identifier;
+use local_oer\logger;
 use local_oer\modules\elements;
 use local_oer\modules\element;
+use tool_opencast\local\api;
+use tool_opencast\local\settings_api;
 
 /**
  * Class module
@@ -43,8 +47,76 @@ class module implements \local_oer\modules\module {
      * @throws \moodle_exception
      */
     public function load_elements(int $courseid): \local_oer\modules\elements {
+        // TODO: Implement behaviour for multiple instances.
+        $settings = settings_api::get_default_ocinstance();
+        $videos = $this->load_videos($courseid, $settings->id);
         $elements = new elements();
+        if (empty($videos)) {
+            return $elements;
+        }
+
+        $instance = settings_api::get_apiurl($settings->id);
+        $creator = "oermod_opencast\module";
+        foreach ($videos as $video) {
+            if ($video->processing_state != 'SUCCEEDED') {
+                // Only show working videos.
+                // TODO check possible states.
+                continue;
+            }
+            $element = new element($creator, element::OERTYPE_EXTERNAL);
+            $identifier = identifier::compose('opencast', $instance,
+                    'video', 'identifier', $video->identifier);
+            $element->set_identifier($identifier);
+            $element->set_origin('opencast');
+            $element->set_title($video->title);
+            $element->set_license(empty($video->license) ? 'unknown' : $video->license);
+            // TODO: What other positions are possible in this array? Is the video url always in key 0?
+            $element->set_source($video->publications[0]->url);
+            $elements->add_element($element);
+        }
         return $elements;
+    }
+
+    /**
+     * Use the opencast API to load all videos for a Moodle course.
+     *
+     * @param int $courseid Moodle course id.
+     * @param int $instanceid Opencast instance id stored in tool_opencast settings.
+     * @return array
+     * @throws \dml_exception
+     * @throws \moodle_exception
+     */
+    private function load_videos(int $courseid, int $instanceid): array {
+        // A course can have more than one series.
+        global $DB;
+        $list = $DB->get_records('tool_opencast_series', ['courseid' => $courseid]);
+        $videos = [];
+        foreach ($list as $series) {
+            $params = [
+                    'sign' => false,
+                    'withacl' => false,
+                    'withmetadata' => false,
+                    'withpublications' => true,
+                    'sort' => [
+                            'start_date' => 'DESC',
+                    ],
+            ];
+
+            $api = new api($instanceid);
+            $response = $api->opencastapi->eventsApi->getBySeries($series->series, $params);
+            $code = $response['code'];
+
+            if ($code != 200) {
+                logger::add($courseid, logger::LOGERROR,
+                        "OERmod opencast: could not reach opencast server. Status Code:$code", 'oermod_opencast');
+                continue;
+            }
+            if (empty($response['body'])) {
+                continue;
+            }
+            $videos = array_merge($videos, $response['body']);
+        }
+        return $videos;
     }
 
     /**
