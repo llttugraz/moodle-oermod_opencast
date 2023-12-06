e98403208b76f56d7e212139f56bb96d3cdec2bd
Christian Ortner <christian.ortner@tugraz.at>
2023-12-06 15:22:57 +0100

    add supported_licences and add write_to_source

--- classes/module.php
+++ classes/module.php
@@ -25,6 +25,7 @@
 
 namespace oermod_opencast;
 
+use block_opencast\local\apibridge;
 use local_oer\identifier;
 use local_oer\logger;
 use local_oer\modules\elements;
@@ -48,6 +49,7 @@ class module implements \local_oer\modules\module {
      */
     public function load_elements(int $courseid): \local_oer\modules\elements {
         // TODO: Implement behaviour for multiple instances.
+        // This will also affect the write back function.
         $settings = settings_api::get_default_ocinstance();
         $videos = $this->load_videos($courseid, $settings->id);
         $elements = new elements();
@@ -60,7 +62,7 @@ class module implements \local_oer\modules\module {
         foreach ($videos as $video) {
             if ($video->processing_state != 'SUCCEEDED') {
                 // Only show working videos.
-                // TODO check possible states.
+                // Possible states: INSTANTIATED, RUNNING, PAUSED, SUCCEEDED, FAILED, SKIPPED, RETRY.
                 continue;
             }
             $element = new element($creator, element::OERTYPE_EXTERNAL);
@@ -69,8 +71,8 @@ class module implements \local_oer\modules\module {
             $element->set_identifier($identifier);
             $element->set_origin('opencast', 'origin', 'oermod_opencast');
             $element->set_title($video->title);
-            // TODO: license mapping will be necessary
-            $element->set_license(empty($video->license) ? 'unknown' : $video->license);
+            $license = $this->match_licence('opencast', $video->license);
+            $element->set_license($license);
             // TODO: What other positions are possible in this array? Is the video url always in key 0?
             $element->set_source($video->publications[0]->url);
             $element->add_information('origin', 'local_oer',
@@ -139,7 +141,105 @@ class module implements \local_oer\modules\module {
      *
      * @param element $element
      * @return void
+     * @throws \coding_exception
+     * @throws \dml_exception
+     * @throws \moodle_exception
      */
     public function write_to_source(\local_oer\modules\element $element): void {
+        $decompose = identifier::decompose($element->get_identifier());
+        if ($decompose->platform != 'opencast' || $decompose->type != 'video' || $decompose->valuetype != 'identifier') {
+            return;
+        }
+        $moodlelicence = $element->get_license();
+        if ($moodlelicence == 'unknown') {
+            return; // Do not update unknown licence.
+        }
+        $settings = settings_api::get_default_ocinstance();
+        $api = new api($settings->id);
+        $licence = $this->match_licence('moodle', $moodlelicence);
+        $update = [
+                'id' => 'license',
+                'value' => $licence,
+        ];
+        $metadata = json_encode([$update]);
+        $type = 'dublincore/episode';
+
+        $response = $api->opencastapi->eventsApi->updateMetadata($decompose->value, $type, $metadata);
+    }
+
+    /**
+     * Match Opencast licences with Moodle active licences and return the result.
+     *
+     * @return array
+     */
+    public function supported_licences(): array {
+        $licences = \license_manager::get_active_licenses_as_array();
+        $result = [];
+        foreach ($this->licence_mapping() as $moodle => $opencast) {
+            if (isset($licences[$moodle])) {
+                $result[] = $moodle;
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Map the Moodle licences to its Opencast counterpart.
+     *
+     * The shortnames of the licenses will be matched, not the visible names.
+     * Only the base licences are matched here.
+     *
+     * TODO: should this be more dynamic for custom licences?
+     * TODO: what happens if some licences are deactivated in opencast?
+     *
+     * @return array
+     */
+    private function licence_mapping(): array {
+        return [
+                'unknown' => '', // Empty string in Opencast, also all other licenses not matchable.
+                'allrightsreserved' => 'ALLRIGHTS',
+                'public' => 'CC0',
+                'cc-4.0' => 'CC-BY',
+                'cc-nc-4.0' => 'CC-BY-NC',
+                'cc-nd-4.0' => 'CC-BY-ND',
+                'cc-nc-nd-4.0' => 'CC-BY-NC-ND',
+                'cc-nc-sa-4.0' => 'CC-BY-NC-SA',
+                'cc-sa-4.0' => 'CC-BY-SA',
+        ];
+    }
+
+    /**
+     * Match a given licence to its counterpart.
+     *
+     * @param string $source Source can be either 'moodle' or 'opencast'.
+     * @param string $licence License shortname string.
+     * @return string
+     * @throws \coding_exception
+     */
+    private function match_licence(string $source, string $licence): string {
+        if (!in_array($source, ['moodle', 'opencast'])) {
+            throw new \coding_exception('Wrong source given, only "moodle" or "opencast" are allowed.');
+        }
+
+        foreach ($this->licence_mapping() as $moodle => $opencast) {
+            if ($source == 'moodle' && $moodle == $licence) {
+                return $opencast;
+            }
+            if ($source == 'opencast' && $opencast == $licence) {
+                return $moodle;
+            }
+        }
+        return $source == 'moodle' ? '' : 'unknown';
+    }
+
+    /**
+     * When an opencast video is released, the video has to be set to be public accessible.
+     * Also, the video should not be deletable for lecturers.
+     *
+     * @return bool
+     */
+    public function set_element_to_release(): bool {
+        // TODO: implement release.
+        return true;
     }
 }
